274109e1c0ae89dc784d0578b0fb0d860c76dda0
Saurya Raj Pandey <sauryap77@gmail.com>
2022-01-20 12:46:40 +0545

    New message model

--- src/ThisIsForCommit.java
+++ src/ThisIsForCommit.java
@@ -1,5 +1,7 @@
 public class ThisIsForCommit {
     public static void main(String[] args) {
         System.out.println("This is  a commit");
+        System.out.println("The new Commit");
+        System.out.println("new ");
     }
 }
